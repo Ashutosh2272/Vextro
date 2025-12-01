f6d42401d4b7b0909918f3e41a2e17d8c2238561
Ashutosh2272 <ashumaurya2511@gmail.com>
2025-12-01 23:35:55 +0530

    add dotenv for environment variable management and implement socket connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,32 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express, { request } from "express";
 import {createServer} from "node:http";
+import connectToSocket from "./controllers/socketManager.js";
 
 import { Server } from "socket.io";
 
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
 import cors from "cors";
 
 const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
 
-app.get("/home", (req, res)=>{
-    return res.json({"hello": "world"})
-})
+app.set("port", process.env.PORT || 8000);
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
 const start = async ()=>{
-    app.listen(8000, ()=>{
+    // app.set('mong_user');
+    const mongoURL = process.env.MONGODB_URI
+    // console.log(mongoURL)
+    const connectionDB = await mongoose.connect(mongoURL)
+    console.log("DB connection name: ", connectionDB.connection.name)
+        server.listen(app.get('port'), ()=>{
         console.log("app listen on port 8000")
     });
 }
